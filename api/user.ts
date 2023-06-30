63c3937219bd40c5ff1e78cd5d345dc06cd77e35
China_quanda <877880098@qq.com>
2023-06-30 15:27:32 +0800

    Revert "Revert "first commit""
    This reverts commit f5b0e34ef34837a2c8bbe2d18c3107974adf9deb.

---
+++ api/user.ts
@@ -0,0 +1,28 @@
+import request from '@/utils/request'
+
+// 已扫描二维码
+export const qrScannedLogin =(data)=> {
+  return request({
+    url: '/app/qrCode/scanned',
+    method: 'post',
+    data
+  })
+}
+
+// 扫码授权确认登录
+export const qrConfirmLogin =(data)=> {
+  return request({
+    url: '/app/qrCode/confirm',
+    method: 'post',
+    data
+  })
+}
+
+// 扫码授权取消登录
+export const qrCancelLogin =(data)=> {
+  return request({
+    url: '/app/qrCode/cancel',
+    method: 'post',
+    data
+  })
+}
---
+++ utils/config.ts
@@ -0,0 +1,9 @@
+export const app = {
+	title:'juejin',
+	homeUrl:'/pages/index/index'
+}
+export const request = {
+	baseUrl:'http://192.168.2.7:7001',
+	timeout:10000,
+	dataType:'json'
+}
