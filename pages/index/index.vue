a320ae21bb27341b41aba92cb3b2cf8e1844ce4f
China_quanda <877880098@qq.com>
2023-07-02 15:49:32 +0800

    首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,40 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<my-nav-bar rightText="搜索" :clickLeft="clickLeft" :clickRight="clickRight" :clickCentre="clickCentre" leftWidth="60rpx" rightWidth="70rpx">
+			<view class="input-view">
+				<uni-icons type="search" size="18" color="#999" />
+				<input disabled class="nav-bar-input" type="text" placeholder="你想搜索点什么" />
+			</view>
+			<block v-slot:left><my-scan size="18" color="#000" /></block>
+		</my-nav-bar>
 	</view>
 </template>
-
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-			
-		}
-	}
+<script setup lang="ts">
+import router from '@/utils/router';
+const clickLeft = () => {};
+const clickRight = () => {
+	console.log('clickRight');
+};
+const clickCentre = () => {
+	console.log('clickCentre');
+	router.push('../search/index');
+};
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="scss" scoped>
+.input-view {
+	display: flex;
+	flex: 1;
+	border-radius: 15px;
+	padding: 0 15px;
+	line-height: 30px;
+	background-color: #f8f8f8;
+}
+.nav-bar-input {
+	height: 30px;
+	width: 100%;
+	line-height: 30px;
+	padding: 0 5px;
+	font-size: 12px;
+}
 </style>
