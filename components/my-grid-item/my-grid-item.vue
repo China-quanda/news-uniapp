84f93f692768a2b6ca949a7af92e4254e4bb0d73
China_quanda <877880098@qq.com>
2023-07-05 00:43:46 +0800

    新增组件

---
+++ components/my-grid-item/my-grid-item.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="grid-item" :style="gridItem"><slot></slot></view>
+</template>
+
+<script setup lang="ts">
+import { watch, reactive } from 'vue';
+const props = defineProps({
+	backgroundColor: {
+		type: String,
+		default: '#fff'
+	},
+	radius: {
+		type: [String, Number],
+		default: 0
+	}
+});
+let gridItem = reactive({
+	'border-radius': '0px',
+	'background-color': '#fff'
+});
+watch(() => {
+	gridItem['background-color'] = props.backgroundColor;
+	gridItem['grid-gap'] = props.radius + 'px';
+});
+</script>
+
+<style lang="scss">
+.grid-item {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 5px;
+}
+</style>
---
+++ components/my-grid/my-grid.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="grid" :style="grid"><slot></slot></view>
+</template>
+
+<script setup lang="ts">
+import { reactive, watch } from 'vue';
+const props = defineProps({
+	backgroundColor: {
+		type: String,
+		default: '#f8f8f8'
+	},
+	gap: {
+		type: [String, Number],
+		default: '5'
+	},
+	columns: {
+		type: [String, Number],
+		default: 4
+	},
+	rows: {
+		type: [String, Number],
+		default: ''
+	},
+	width: {
+		type: String,
+		default: '1fr'
+	},
+	height: {
+		type: String,
+		default: ''
+	}
+});
+let grid = reactive({
+	'grid-template-columns': 'repeat(4,1fr)',
+	'grid-gap': '5px',
+	'grid-template-rows': 'repeat(1fr)',
+	'background-color': '#f8f8f8'
+});
+watch(() => {
+	grid['background-color'] = props.backgroundColor;
+	grid['grid-gap'] = props.gap + 'px';
+	grid['grid-template-columns'] = `repeat(${props.columns},${props.width})`;
+	grid['grid-template-rows'] = `repeat(${props.rows},${props.height})`;
+});
+</script>
+
+<style lang="scss">
+.grid {
+	display: grid;
+	padding: 12px;
+	// margin: 12px;
+	text-align: center;
+	box-sizing: border-box;
+}
+</style>
