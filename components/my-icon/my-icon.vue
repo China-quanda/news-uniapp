f09f79f855b663378e7752df584bb943e665ec3c
China_quanda <877880098@qq.com>
2023-07-05 22:18:38 +0800

    新增icon图标组件

---
+++ components/my-icon/my-icon.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
+
+const props = defineProps({
+	type: {
+		type: String,
+		default: 'font-class'
+	},
+	className: {
+		type: String,
+		default: ''
+	},
+	icon: {
+		type: String,
+		require: true
+	},
+	size: {
+		type: [String, Number],
+		default: '16'
+	},
+	color: {
+		type: String,
+		default: '#8a8a8a'
+	}
+});
+const style = reactive({
+	icon: props.icon,
+	'font-size': props.size + 'px',
+	color: props.color
+});
+// 给图标添加上类名
+const svgClass = computed(() => {
+	if (props.className) {
+		return `svg-icon ${props.className}`;
+	}
+	return 'svg-icon';
+});
+</script>
+
+<template>
+	<view class="my-icon">
+		<template v-if="type == 'font-class'">
+			<i :class="'iconfont ' + icon" :style="style"><slot></slot></i>
+		</template>
+		<template v-if="type == 'svg'">
+			<svg :class="svgClass" aria-hidden="true" :style="style"><use :xlink:href="'#' + icon" :fill="color" /></svg>
+		</template>
+	</view>
+</template>
+<style scoped>
+.my-icon {
+	display: inline-block;
+}
+.svg-icon {
+	width: 1em;
+	height: 1em;
+	position: relative;
+	fill: currentColor;
+	vertical-align: -2px;
+}
+</style>
