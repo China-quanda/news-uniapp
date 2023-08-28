9716cfb7c9fa8d656538c7b7d2d40ce6e0961f43
China_quanda <877880098@qq.com>
2023-08-28 08:46:41 +0800

    edit

--- utils/request.ts
+++ utils/request.ts
@@ -3,9 +3,12 @@
 	const request = config => {
 		// 是否隐藏加载
 		if (config.data && config.data.Loading) {
-			uni.showLoading({
-				title: '加载中'
-			});
+			uni.showLoading({ title: '加载中' });
+			let time = 0
+			var myInterval = setInterval(()=>{
+				time = time + 1000
+				if(time >= 6000) uni.showLoading({title: '当前网络较慢'});
+			},1000)	
 		}
 		return new Promise((resolve, reject) => {
 			uni.request({
@@ -19,66 +22,40 @@
 				},
 				dataType: requestConfig.dataType,
 				success: res => {
-					// 是否隐藏加载
-					if (config.data && config.data.Loading) {
-						uni.hideLoading();
-					}
 					// 请求失败
 					if (res.errMsg != 'request:ok') {
 						reject('请求失败')
-						return uni.showToast({
-							title: res.errMsg,
-							icon: 'none',
-							duration: 2000
-						});
+						return uni.showToast({ title: res.errMsg, icon: 'none', duration: 2000 });
 					}
 					if (res.data.code == 200 || res.data.code == 0) {
 						resolve(res.data);
 					} else if (res.data.code == 400) {
 						reject('400')
-						return uni.showToast({
-							icon: 'none',
-							title: res.data.msg
-						});
+						return uni.showToast({ icon: 'none', title: res.data.msg });
 					} else if (res.data.code == 401) {
 						reject('401')
-						uni.showToast({
-							icon: 'none',
-							title: '认证失败,请重新登录'
-						});
+						uni.showToast({ icon: 'none', title: '认证失败,请重新登录' });
 						setTimeout(() => {
-							uni.reLaunch({
-								url: '/pages/index/login'
-							});
+							uni.reLaunch({ url: '/pages/index/login' });
 						}, 1500)
 					} else if (res.data.code == 403) {
 						reject('403')
-						return uni.showToast({
-							icon: 'none',
-							title: '无权限操作'
-						});
+						return uni.showToast({ icon: 'none', title: '无权限操作' });
 					} else {
 						reject(res.data)
-						uni.showToast({
-							title: res.data.msg,
-							icon: 'none',
-							duration: 2000
-						});
+						uni.showToast({ title: res.data.msg, icon: 'none', duration: 2000 });
 					}
 				},
 				fail: error => {
 					reject(error)
-					// 是否隐藏加载
-					if (config.data && config.data.Loading) {
-						uni.hideLoading();
-					}
-					uni.showToast({
-						title: '请求失败,请稍后再试',
-						icon: 'none',
-						duration: 2000
-					});
+					uni.showToast({ title: '请求失败,请稍后再试', icon: 'none', duration: 2000 });
 				},
 				complete: res => {
+					// 是否隐藏加载
+					if(requestData.data && requestData.data.Loading) {
+						uni.hideLoading();
+						clearInterval(myInterval)
+					}
 					if (res.statusCode == '500') {
 						reject('服务器开小差了,请稍后再试')
 						uni.showToast({
