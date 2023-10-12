a7900c4a6b303b0933bc83c32df03316b84d9e39
China_quanda <877880098@qq.com>
2023-10-12 09:19:02 +0800

    add

---
+++ components/my-badge/badge.md
@@ -0,0 +1,71 @@
+#Badge 徽标
+
+**介绍**
+在右上角展示徽标数字或小红点。
+
+**使用**
+
+```
+
+	<!-- 基础用法 -->
+	
+	设置 value 属性后，Badge 会在子元素的右上角显示对应的徽标，可自定义徽标内容 
+ <my-badge value="2">
+ 	<button>消息</button>
+ </my-badge>
+ 
+ 
+ 也可以通过 isDot 来显示小红点。不展示数字，只有一个小点
+ <my-badge isDot>
+ 	<button>消息</button>
+ </my-badge>
+ 
+ 
+ 最大值
+ 设置 max 属性后，当 value 的数值超过最大值时，会自动显示为 {max}+。
+ <my-badge value="2" :max="21">
+ 	<button>消息</button>
+ </my-badge>
+ 
+ 
+ 自定义颜色
+ 通过 color 属性来设置徽标的颜色。
+ <my-badge value="2" :max="21" color="red">
+ 	<button>消息</button>
+ </my-badge>
+ 
+ 
+ 自定义背景颜色
+ 通过 bgColor="#3c9cff" 属性来设置徽标的背景颜色。
+ <my-badge value="2" :max="21" bgColor="#3c9cff">
+ 	<button>消息</button>
+ </my-badge>
+ 
+ 
+ 数据为0时是否显示
+ 通过 showZero 属性来设置徽标为0时是否显示
+ <my-badge value="2" :max="21" showZero>
+ 	<button>消息</button>
+ </my-badge>
+ 
+ 
+ 自定义徽标位置
+ 通过 position 属性来设置徽标的位置。
+ <my-badge value="2" :max="21" position="top-left">
+ 	<button>消息</button>
+ </my-badge>
+	
+```
+
+**Props**
+| 参数 | 说明 | 类型 | 默认值 | 是否必填
+| ----- | ----------------- | ------ | ------ |------ |
+| value | 显示值  | string,number | '' | 否 |
+| max | 最大值，超过最大值会显示 '{max}+'，要求 value 是 Number 类型 | number | '-' | 否 |
+| isDot | 不展示数字，只有一个小点 | Boolean | 'false' | 否 |
+| showZero | 当数值为 0 时，是否展示 Badge  | Boolean | 'false' | 否 |
+| color | 文字颜色 | string | '#fff' | 否 |
+| bgColor | 背景颜色 | string | '#f56c6c' | 否 |
+| position | 徽标位置，可选值为 top-left bottom-left bottom-right | string | 'top-right' | 否 |
+
+position还未实现
---
+++ components/my-badge/my-badge.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+import { watch, ref } from 'vue';
+
+const props = defineProps({
+	value: {
+		type: [String, Number],
+		default: 0
+	},
+	max: {
+		type: Number
+	},
+	isDot: {
+		type: Boolean,
+		default: false
+	},
+	showZero: {
+		type: Boolean,
+		default: false
+	},
+	color: {
+		type: String,
+		default: '#fff'
+	},
+	bgColor: {
+		type: String,
+		default: '#f56c6c'
+	},
+	position: {
+		type: String,
+		default: 'top-right'
+	}
+});
+
+let content = ref(props.value);
+
+watch(
+	() => props,
+	() => {
+		if (typeof props.value === 'number' && props.max) {
+			if (props.value > props.max) {
+				content.value = `${props.max}+`;
+			} else {
+				content.value = props.value;
+			}
+		} else {
+			content.value = props.value;
+		}
+	},
+	{ deep: true, immediate: true }
+);
+</script>
+
+<template>
+	<view class="badge">
+		<text v-if="value || showZero" :class="['badge-content', isDot ? 'badge-dot' : '', position]">{{ content }}</text>
+		<slot></slot>
+	</view>
+</template>
+
+<style scoped lang="scss">
+.badge {
+	position: relative;
+	vertical-align: middle;
+	display: inline-block;
+	.badge-content {
+		position: absolute;
+		background-color: v-bind('props.bgColor');
+		border-radius: 100px;
+		color: v-bind('props.color');
+		display: inline-block;
+		font-size: 12px;
+		height: 18px;
+		line-height: 18px;
+		padding: 0 6px;
+		text-align: center;
+		white-space: nowrap;
+	}
+	.top-left {
+		top: 0;
+		left: 0;
+		transform: translate(-50%, -50%);
+	}
+	.top-right {
+		top: 0;
+		right: 0;
+		transform: translate(50%, -50%);
+	}
+	.bottom-left {
+		bottom: 0;
+		left: 0;
+		transform: translate(-50%, 50%);
+	}
+	.bottom-right {
+		bottom: 0;
+		right: 0;
+		transform: translate(50%, 50%);
+	}
+
+	.badge-dot {
+		height: 8px;
+		max-height: 8px;
+		width: 8px;
+		max-width: 8px;
+		padding: 0;
+		color: v-bind('props.bgColor');
+		overflow: hidden;
+	}
+}
+</style>
