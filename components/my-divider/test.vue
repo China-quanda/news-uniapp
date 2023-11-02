2004786f7147688e49af7ed8cf3365c0b5b2666c
China_quanda <877880098@qq.com>
2023-11-02 09:03:45 +0800

    add

---
+++ components/my-divider/test.vue
@@ -0,0 +1,27 @@
+<template>
+	
+	<my-gap></my-gap>
+	
+	<my-divider />
+	
+	<my-divider textPosition="center" text="文本"/>
+	
+	<my-divider textPosition="left" >
+		left
+	</my-divider>
+	
+	<my-divider textPosition="right" >
+		right
+	</my-divider>
+	
+	<my-divider textPosition="center" dot />
+	
+		文本
+	<my-divider vertical/>
+		文本
+	<my-divider vertical/>
+		文本
+	<my-divider vertical/>
+
+</template>
+
