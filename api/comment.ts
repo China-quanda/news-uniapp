c24c51ecf3fa13de5babeb979263fa7c978928d0
China-quanda <877880098@qq.com>
2024-01-19 17:48:05 +0800

    edit

---
+++ api/comment.ts
@@ -0,0 +1,9 @@
+import request from '@/utils/request'
+// 获取用户评论列表
+export const getUserCommentlist =(query:any):Promise<any>=> {
+  return request({
+    url: '/article/comment/getUserCommentlist',
+    method: 'GET',
+		params:query
+  })
+}
---
+++ api/search.ts
@@ -0,0 +1,9 @@
+import request from '@/utils/request'
+// 获取热门搜索列表
+export const getHotSearchlist =(query:any):Promise<any>=> {
+  return request({
+    url: '/article/search/getHotSearch',
+    method: 'GET',
+		params:query
+  })
+}
