4ad6115baa12053c889e0e6389561e82b926bc16
China-quanda <877880098@qq.com>
2024-03-15 17:59:54 +0800

    新增微信小程序支付功能

--- pages/pay/pay.vue
+++ pages/pay/pay.vue
@@ -1,44 +1,65 @@
 <template>
-  <view>
-		11
-   <button @click="pay">支付</button> 
-  </view>
+	<view>
+		<button @click="wechatPay">微信支付</button>
+	</view>
 </template>
 
 <script setup lang="ts">
-	const pay = ()=>{
-		console.log('pay');
-		uni.login({
-			provider: "weixin",
-			onlyAuthorize: true, // 微信登录仅请求授权认证
-			success({code}){
-				console.log('code',code);
-				uni.request({
-					url:'http://127.0.0.1:7002/api/weChatMp/login',
-					method:'POST',
-					data: { code },
-					success(lres){
-						console.log('lres',lres.data);
-						
-						wx.requestPayment({
-							timeStamp: res.data.timeStamp,
-							nonceStr: res.data.nonceStr,
-							package: res.data.packageValue,
-							signType: res.data.signType,
-							paySign: res.data.paySign,
-						})
-					},
-					fail(lerr) {
-						console.log('lerr',lerr);
-					}
-				})
-			},
-			fail (err) {
-				console.log('err',err);
-		        // 登录授权失败
-		        // err.code是错误码
-		    }
-		})
-	}
-
+const wechatPay = () => {
+	console.log('pay');
+	uni.login({
+		provider: "weixin",
+		success({ code }) {
+			uni.request({
+				url: 'http://192.168.0.107:7001/api/weChatMp/login',
+				method: 'POST',
+				data: { code },
+				success(lres) {
+					const openId = lres.data.data.openId
+					console.log('lres - openId', openId);
+					uni.request({
+						url: 'http://192.168.0.107:7001/api/weChatMp/order',
+						method: 'POST',
+						data: { openId },
+						success(ores) {
+							console.log('ores', ores.data);
+							uni.requestPayment({
+								provider: 'wxpay',
+								orderInfo: ores.data,
+								timeStamp: ores.data.timeStamp,
+								nonceStr: ores.data.nonceStr,
+								package: ores.data.package,
+								signType: ores.data.signType,
+								paySign: ores.data.paySign,
+								success(payres) {
+									console.log('payres', payres);
+									uni.showModal({
+										title: '支付成功',
+										content: '支付成功',
+										showCancel: false,
+										success() {
+											uni.navigateBack();
+										}
+									})
+								},
+								fail(parerr) {
+									console.log('parerr', parerr, JSON.stringify(parerr));
+								}
+							});
+						},
+						fail(oerr) {
+							console.log('lerr', oerr);
+						}
+					})
+				},
+				fail(lerr) {
+					console.log('lerr', lerr);
+				}
+			})
+		},
+		fail(loginErr) {
+			console.log('loginErr', loginErr);
+		}
+	})
+}
 </script>
