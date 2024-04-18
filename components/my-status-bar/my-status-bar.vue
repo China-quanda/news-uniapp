5b46512a621fb1e4cf1b50edb7622182fd810956
China-quanda <877880098@qq.com>
2024-04-18 21:55:49 +0800

    新增状态栏组件

---
+++ components/my-status-bar/my-status-bar.vue
@@ -0,0 +1,21 @@
+<template>
+  <view :style="{ height: statusBarHeight + 'px', backgroundColor: bgColor }" class="my-status-bar w-full">
+    <slot />
+  </view>
+</template>
+<script lang="ts">
+export default { name: 'statusBar' }
+</script>
+<script setup lang="ts">
+const statusBarHeight = ref(0)
+onMounted(() => {
+  const systemInfo: GetSystemInfoResult = uni.getSystemInfoSync()
+  statusBarHeight.value = systemInfo.statusBarHeight! || 0
+})
+defineProps({
+  bgColor: {
+    type: String,
+    default: 'transparent'
+  }
+})
+</script>
