bc6c07a345736d9694d309f1bff08ffa615f08d5
China-quanda <877880098@qq.com>
2024-04-18 21:55:29 +0800

    新增公共页面

---
+++ pages/common/textview/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<uni-card class="view-title font-500" :title="title">
+		<text class="uni-body view-content">{{ content }}</text>
+	</uni-card>
+</template>
+
+<script setup lang="ts">
+const title = ref('');
+const content = ref('');
+onLoad((options) => {
+	title.value = options.title;
+	content.value = options.content;
+	uni.setNavigationBarTitle({
+		title: options.title
+	});
+});
+</script>
+
+<style scoped>
+page {
+	background-color: #ffffff;
+}
+
+.view-content {
+	font-size: 26rpx;
+	padding: 12px 5px 0;
+	color: #333;
+	line-height: 24px;
+	font-weight: normal;
+}
+</style>
---
+++ pages/common/webview/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<web-view v-if="params.url" :webview-styles="webviewStyles" :src="`${params.url}`"></web-view>
+</template>
+
+<script setup lang="ts">
+let params = reactive({
+	url: null
+})
+let webviewStyles = reactive({
+	progress: {
+		color: "#FF3333"
+	}
+})
+onLoad((event) => {
+	params = event
+	if (event.title) {
+		uni.setNavigationBarTitle({
+			title: event.title
+		})
+	}
+})
+
+</script>
