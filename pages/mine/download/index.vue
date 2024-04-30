86ed4ac8f4e182f8ddd44277b106de3803fc2a2c
China-quanda <877880098@qq.com>
2024-04-30 21:06:35 +0800

    新增下载页面

---
+++ pages/mine/download/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class='download'>
+		<!-- <u-empty mode="data"  text="暂无下载内容"/> -->
+		<text class="iconfont icon-xiazai"></text>
+		<text class="text">暂无下载内容</text>
+	</view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+.download {
+
+	@apply flex flex-col flex-items-center gap-y-20px translate-y-25vh;
+	// transform: translateY(25vh);
+
+	.iconfont {
+		@apply text-40px;
+	}
+
+	.text {
+		@apply text-14px;
+	}
+}
+</style>
