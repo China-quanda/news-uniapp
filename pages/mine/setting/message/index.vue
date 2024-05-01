71044aef38511c36594eb0e068f0d02f4d141e69
China-quanda <877880098@qq.com>
2024-05-01 13:37:18 +0800

    新增消息设置页面

---
+++ pages/mine/setting/message/index.vue
@@ -0,0 +1,236 @@
+<template>
+	<view class="message">
+
+		<view class="section">互动消息</view>
+		<view class="cell-group">
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">评论</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">所有人</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">新增粉丝</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">所有人</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">点赞</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">所有人</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell">
+				<view class="cell-left">
+					<text class="title">@我</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">所有人</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+		</view>
+
+		<view class="section">私信消息</view>
+		<view class="cell-group">
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">谁可以私信我</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell">
+				<view class="cell-left">
+					<text class="title">陌生人消息</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">聚合消息</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+		</view>
+
+		<view class="section">其他消息</view>
+		<view class="cell-group">
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">问答</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">新回答与所有回答邀请</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">圈子</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">话题邀请</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">作者成长助手</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">创作通知</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">头条小组</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">活动通知</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">功能通知</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">订阅通知</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">头条号</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">头条百科</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+			<view class="cell is-border">
+				<view class="cell-left">
+					<text class="title">钱包通知</text>
+				</view>
+				<view class="cell-right">
+					<text class="desc">默认</text>
+					<text class="iconfont icon-xiangyoujiantou text-14px text-#a3a6a8"></text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup lang="ts">
+</script>
+<style lang="scss" scoped>
+page,
+.message {
+	background-color: rgb(240, 240, 240);
+}
+
+
+.section {
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-end;
+	padding: 10px 15px;
+	margin: 0;
+	color: #bebec4;
+	font-size: 14px;
+	font-weight: 400;
+}
+
+
+.gap {
+	@apply h8px;
+}
+
+.cell-group {
+	@apply bg-white px-12px;
+
+	.cell {
+		@apply flex flex-items-center py-10px;
+
+		.cell-left {
+			@apply flex flex-col flex-1;
+
+			.title {
+				@apply text-13.5px;
+			}
+
+			.subtitle {
+				@apply text-13px text-blueGray;
+			}
+		}
+
+		.cell-right {
+			@apply flex flex-items-center gap-x-3px;
+
+			.desc {
+				@apply text-13px text-blueGray;
+			}
+		}
+	}
+
+	.is-border {
+		@apply border-b-1px border-b-solid border-b-coolGray-100;
+	}
+}
+</style>
