5780f9e486d5fd86fae320d9be8dbb8a089d2df0
China-quanda <877880098@qq.com>
2024-05-01 17:03:47 +0800

    新增登录设备管理页面

---
+++ pages/mine/setting/account/device/login-device.vue
@@ -0,0 +1,66 @@
+<template>
+	<view class='login-device'>
+
+		<view class="tisi">
+			<view class="zaixian">在线设备（10）</view>
+			<view class="miaosu">
+				以下是近期登录的设备，若非本人操作，请及时移除。移除后账号将从这个设备登出，再次登录时需要进行身份验证。
+			</view>
+		</view>
+
+		<view class="item" @tap="router.push('device-info')">
+			<view class="title">
+				<text class="iconfont icon-shouji text-16px">iPhone X</text>
+			</view>
+			<view class="con flex flex-col">
+				<text class="text-14px text-#ccc">登录应用：今日头条</text>
+				<text class="text-14px text-#ccc">最近活跃：2021-02-06 15:53</text>
+				<text class="iconfont icon-xiangyoujiantou icon-view text-18px"></text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup lang="ts">
+import router from '@/utils/router';
+</script>
+<style lang="scss" scoped>
+.login-device {
+	background-color: #F8F8F8;
+}
+
+.tisi {
+	padding: 15px;
+
+	.zaixian {
+		font-size: 18px;
+		margin-bottom: 10px;
+
+	}
+
+	.miaosu {
+		color: #554d4d;
+		font-size: 14px;
+	}
+}
+
+.item {
+	padding: 15px;
+	background-color: #fff;
+	border-bottom: 0.5px solid #ccc;
+
+	.title {
+		margin-bottom: 10px;
+	}
+
+	.con {
+		position: relative;
+
+		.icon-view {
+			position: absolute;
+			top: 0;
+			right: 0;
+		}
+	}
+}
+</style>
