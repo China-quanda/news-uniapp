dba898177706feb3bd7a9aa36c2e99400576cf8f
China-quanda <877880098@qq.com>
2024-05-07 09:13:21 +0800

    add MySafeAreaInsetsBottom

---
+++ components/my-safeAreaInsetsBottom/my-safeAreaInsetsBottom.vue
@@ -0,0 +1,19 @@
+<template>
+	<view :style="{ height: safeAreaInsetsBottom + 'px', backgroundColor: bgColor }"
+		class="my-safeAreaInsets-bottom w-full">
+	</view>
+</template>
+<script lang="ts">
+export default { name: 'MySafeAreaInsetsBottom' };
+</script>
+<script setup lang="ts">
+import { useAppStore } from '@/store/app';
+const appStore = useAppStore();
+const safeAreaInsetsBottom = appStore.systemInfo.safeAreaInsets.bottom || 0;
+defineProps({
+	bgColor: {
+		type: String,
+		default: 'transparent'
+	}
+});
+</script>
